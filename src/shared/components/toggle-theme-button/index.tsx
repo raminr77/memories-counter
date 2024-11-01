e0a8fba7879e89c65d01c44523f64d2177cd6ba0
Ramin <39030920+raminr77@users.noreply.github.com>
2024-11-01 15:02:39 +0100

    feat: update navigation color with theme

--- src/shared/components/toggle-theme-button/index.tsx
+++ src/shared/components/toggle-theme-button/index.tsx
@@ -6,8 +6,26 @@ import { appSelectors } from 'shared/redux/app/app-selectors';
 
 export function ToggleThemeButton({ className }: GCommonComponentProperties) {
     const dispatch = useDispatch();
-    const { darkMode } = useSelector(appSelectors.appData);
-    const toggleTheme = () => dispatch(toggleDarkMode(!darkMode));
+    const { darkMode } = useSelector(appSelectors.appData) as GApp;
+
+    const updateBrowserColor = (isDarkMode: boolean) => {
+        const color = isDarkMode ? '#1E2A3B' : '#F1F5F9';
+
+        const themeColorTag: HTMLMetaElement | null = document.querySelector('meta[name="theme-color"]');
+        const tileColorTag: HTMLMetaElement | null = document.querySelector('msapplication-TileColor');
+        const navigationColor: HTMLMetaElement | null = document.querySelector('msapplication-navbutton-color');
+
+        if (themeColorTag && tileColorTag && navigationColor) {
+            themeColorTag.content = color;
+            tileColorTag.content = color;
+            navigationColor.content = color;
+        }
+    };
+
+    const toggleTheme = () => {
+        updateBrowserColor(darkMode);
+        dispatch(toggleDarkMode(!darkMode));
+    };
 
     return (
         <button
--- src/shared/redux/app/app-selectors.ts
+++ src/shared/redux/app/app-selectors.ts
@@ -2,7 +2,7 @@ import { createDraftSafeSelector } from '@reduxjs/toolkit';
 
 const appData = createDraftSafeSelector(
     (state: { APP: GApp }) => state.APP,
-    (state) => ({
+    (state: GApp) => ({
         ...state
     })
 );
